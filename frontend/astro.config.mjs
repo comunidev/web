78891d8683ac0fea4cd93c94813eff01ebf25b84
Rodrigo Carranza <rodrigocarranza@outlook.com>
2023-10-25 19:38:46 -0500

    migrated to pandacss

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,34 @@
 import { defineConfig } from "astro/config";
-import UnoCSS from "@unocss/astro";
 import solidJs from "@astrojs/solid-js";
-
+import { defineConfig as viteDefineConfig } from "vite";
 import mdx from "@astrojs/mdx";
+import pandacss from '@pandacss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: true,
-    extractors: []
-  }), solidJs(), mdx()],
+  integrations: [solidJs(), mdx(), pandacss()],
+  vite: viteDefineConfig({
+    plugins: [
+      /*
+      Uncomment the following line to enable solid-devtools.
+      For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+      */
+      // devtools(),
+    ],
+    server: {
+      port: 3000,
+      proxy: {
+        '/socket.io': {
+          target: 'http://localhost:4500',
+          changeOrigin: true,
+          ws: true,
+        }
+      }
+    },
+    build: {
+      target: 'esnext',
+    },
+  }),
   markdown: {
     shikiConfig: {
       theme: "github-dark",
---
+++ frontend/panda.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, defineGlobalStyles } from "@pandacss/dev"
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx,astro}", "./pages/**/*.{js,jsx,ts,tsx,astro}"],
+
+  // Files to exclude
+  exclude: [],
+  globalCss: defineGlobalStyles({
+    "h1,h2,h3,h4,h5,p,a,ul,li,span": {
+      fontFamily: "Fira Sans, sans-serif",
+    },
+    "code, code *": {
+      fontFamily: "Fira Code, consolas, monospace",
+    },
+  }),
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        colors: {
+          "brand-bg": { value: "#010F1A" },
+          "brand-primary": { value: "#1AD56C" },
+          "brand-secondary": { value: "#E6F36E" },
+          "accent-yellow": { value: "#E7EE4E" },
+          "accent-lime": { value: "#4EEE7E" },
+          "accent-cyan": { value: "#4EE7EE" },
+          "accent-pink": { value: "#EE4EE7" },
+        },
+        sizes: {
+          xs: { value: "20rem" },
+          sm: { value: "24rem" },
+          md: { value: "28rem" },
+          lg: { value: "32rem" },
+          xl: { value: "36rem" },
+          "2xl": { value: "42rem" },
+          "3xl": { value: "48rem" },
+          "4xl": { value: "56rem" },
+          "5xl": { value: "64rem" },
+          "6xl": { value: "72rem" },
+          "7xl": { value: "80rem" },
+          "8xl": { value: "90rem" },
+        }
+      }
+    }
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+
+
+})
